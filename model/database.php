ade228d6e9afc3b56d48919e1eda3abfc97c87b7
ankan basu <ab936@afsconnect1.njit.edu>
2017-03-02 21:09:05 -0500

    updated DSN and other details

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=ab936';
+    $username = 'ab936';
+    $password = 'Debolina2024';
 
     try {
         $db = new PDO($dsn, $username, $password);
