b729d650717ac03f68c134cfe3a60bc98135f92d
avalon24 <ankan.basu85@gmail.com>
2017-03-03 12:46:29 -0500

    Added action=list_categories

--- product_manager/index.php
+++ product_manager/index.php
@@ -52,4 +52,15 @@ if ($action == 'list_products') {
         header("Location: .?category_id=$category_id");
     }
 }    
+if ($action == 'list_categories') {
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+    $category_name = get_category_name($category_id);
+    $categories = get_categories();
+    $products = get_products_by_category($category_id);
+    include('category_list.php');
+}
 ?>
